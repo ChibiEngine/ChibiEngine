5b112477ab10978f096dfb0d4bf743c8b096cf95
Feavy <quentin071100@live.fr>
2022-12-16 19:24:41 +0100

    refactor: change scene's updateList to updateSet

--- src/engine/game/Scene.ts
+++ src/engine/game/Scene.ts
@@ -7,14 +7,14 @@ export default abstract class Scene extends Node implements Updatable {
   public game: Game;
   private camera: Camera;
 
-  private readonly updateList: Updatable[] = [];
+  private readonly updateSet: Set<Updatable> = new Set();
 
   public get scene() {
     return this;
   }
 
   public update(delta: number) {
-    for (const updatable of this.updateList) {
+    for (const updatable of this.updateSet) {
       updatable.update(delta);
     }
     this._update(delta);
@@ -23,6 +23,10 @@ export default abstract class Scene extends Node implements Updatable {
   protected abstract _update(delta: number): void;
 
   public addUpdatable(param: Updatable) {
-    this.updateList.push(param);
+    this.updateSet.add(param);
+  }
+
+  public removeUpdatable(param: Updatable) {
+    this.updateSet.delete(param);
   }
 }
