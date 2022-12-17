168fd800f6570d1a023864a4388290fac67fb4fd
Feavy <quentin071100@live.fr>
2022-12-17 19:26:36 +0100

    Update tweens example

--- src/test/ExampleTweens.ts
+++ src/test/ExampleTweens.ts
@@ -2,13 +2,24 @@ import bunnyURL from "./assets/bunny.png?url";
 import Scene from "../engine/game/Scene";
 import Sprite from "../engine/node/Sprite";
 import Image from "../engine/resource/Image";
-import Move from "../engine/tween/Move";
-import {BounceOut} from "../engine/math/easing/Bounce";
+import {MoveBy} from "../engine/tween/Move";
+import Easing from "../engine/math/easing/Easing";
+import Sequence from "../engine/tween/Sequence";
+import Callback from "../engine/tween/Callback";
+import Delay from "../engine/tween/Delay";
 
 export default class ExampleTweens extends Scene {
   protected async _create() {
     const sprite = await this.add(new Sprite(new Image(bunnyURL))).setPosition(this.game.screen.center);
-    sprite.play(Move.by(0, 200).easing(new BounceOut()).duration(2000));
+    sprite.play(new Sequence(
+            new MoveBy(0, 200).easing(Easing.bounceOut).duration(2000),
+            new Callback(() => {console.log("callback")}),
+            new Delay(3000),
+            new Callback(() => {console.log("Delay expired")}),
+            new MoveBy(0, -200).easing(Easing.bounceOut).duration(2000),
+            new Callback(() => {console.log("Finished")})
+        )
+    );
   }
 
   protected _update(delta: number): void {
