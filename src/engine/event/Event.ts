9915d77ce738ec4c793e481c54a48d2e4da435eb
Feavy <quentin071100@live.fr>
2023-03-15 21:02:42 +0100

    Add sugar for Event: add callback by calling the event.

--- src/engine/event/Event.ts
+++ src/engine/event/Event.ts
@@ -1,5 +1,20 @@
-export default class Event<T> {
+class EventImpl<T> extends Function {
   private listeners: EventListener<T>[] = [];
+
+  constructor() {
+    super();
+    return new Proxy(this, {
+      apply (target, thisArg, args) {
+        const callback = args[0];
+        // An empty call is treated as a read
+        if (!callback || typeof callback !== "function") {
+          throw new Error("Event must be called with a callback callback parameter.");
+        }
+
+        target.subscribe(callback);
+      }
+    })
+  }
 
   public subscribe(callback: (value: T) => void): EventListener<T> {
     const listener = new EventListener(this, callback);
@@ -32,8 +47,13 @@ export default class Event<T> {
   }
 }
 
+declare type Event<T> = EventImpl<T> & ((callback: (val: T) => void) => EventListener<T>);
+
+const Event: new <T>() => Event<T> = EventImpl as any;
+export default Event;
+
 export class EventListener<T> {
-  constructor(private readonly event: Event<T>, public readonly callback: (value: any) => void) {
+  constructor(private readonly event: EventImpl<T>, public readonly callback: (value: any) => void) {
   }
 
   public unsubscribe() {
