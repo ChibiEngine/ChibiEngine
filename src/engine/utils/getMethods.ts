899be273a6c07739bce51d0c404feed29a14f827
Feavy <quentin071100@live.fr>
2023-03-26 01:06:41 +0100

    Fix sonar issue.

--- src/engine/utils/getMethods.ts
+++ src/engine/utils/getMethods.ts
@@ -2,7 +2,7 @@ export default function getMethods<T>(obj: T): (keyof T)[] {
   let properties = new Set()
   let currentObj = Object.getPrototypeOf(obj)
   do {
-    Object.getOwnPropertyNames(currentObj).map(item => properties.add(item))
+    Object.getOwnPropertyNames(currentObj).forEach(item => properties.add(item))
   } while ((currentObj = Object.getPrototypeOf(currentObj)) && Object.getPrototypeOf(currentObj))
   return [...properties.keys()].filter(item => item !== "constructor") as (keyof T)[]
 }
