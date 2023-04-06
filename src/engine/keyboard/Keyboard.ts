62062cba50642b0d95287eb7288bee5882d217a4
Feavy <quentin071100@live.fr>
2023-04-06 20:14:27 +0200

    Keyboard: add wasKeyDown and downKeys functions

--- src/engine/keyboard/Keyboard.ts
+++ src/engine/keyboard/Keyboard.ts
@@ -1,25 +1,39 @@
 import Event from "../event/Event";
 
 namespace Keyboard {
-    const downKeys: Set<string> = new Set();
+    const _downKeys: Set<string> = new Set();
+    const _downKeysTime: Map<string, number> = new Map();
 
     export const onKeyDown: Event<KeyboardEvent> = new Event();
     export const onKeyUp: Event<KeyboardEvent> = new Event();
     export const onKeyPressed: Event<KeyboardEvent> = new Event();
 
     document.addEventListener("keydown", (e: KeyboardEvent) => {
-        downKeys.add(e.key);
+        if(_downKeys.has(e.key)) {
+            return;
+        }
+        _downKeys.add(e.key);
+        _downKeysTime.set(e.key, performance.now());
         onKeyDown.trigger(e);
     });
 
     document.addEventListener("keyup", (e: KeyboardEvent) => {
-        downKeys.delete(e.key);
+        _downKeys.delete(e.key);
         onKeyUp.trigger(e);
         onKeyPressed.trigger(e);
+        _downKeysTime.set(e.key, performance.now()-1);
     });
 
-    export function isKeyDown(arrowUp: string) {
-        return downKeys.has(arrowUp);
+    export function downKeys() {
+        return _downKeys;
+    }
+
+    export function isKeyDown(key: string) {
+        return _downKeys.has(key);
+    }
+
+    export function wasKeyDown(key: string, dt: number) {
+        return isKeyDown(key) || _downKeysTime.has(key) && performance.now() - _downKeysTime.get(key) < dt;
     }
 }
 
