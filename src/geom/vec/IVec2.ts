70e0f96e3c3edb5926784044adbbedeae7fc8093
Feavy <quentin071100@live.fr>
2024-10-01 11:48:59 +0200

    Split Vec2 into interface + class

---
+++ src/geom/vec/IVec2.ts
@@ -0,0 +1,6 @@
+// types-only
+
+export default interface IVec2 {
+  x: number;
+  y: number;
+}
--- src/geom/vec/Vec2.ts
+++ src/geom/vec/Vec2.ts
@@ -1,4 +1,10 @@
-export default interface Vec2 {
-  x: number;
-  y: number;
+import IVec2 from "./IVec2";
+
+export default class Vec2 implements IVec2 {
+  public static readonly ZERO = new Vec2(0, 0);
+
+  public constructor(public readonly x: number = 0, public readonly y: number = x) {
+
+  }
+
 }
